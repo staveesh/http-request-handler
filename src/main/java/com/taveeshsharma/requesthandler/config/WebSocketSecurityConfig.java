0155699b0843b33027143621c9a64d83ccd32ca9
staveesh <sharmataveesh@yahoo.in>
2021-04-02 21:13:26 +0200

    CORS issue check

---
+++ src/main/java/com/taveeshsharma/requesthandler/config/WebSocketSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.taveeshsharma.requesthandler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+}
