e9798b838994ff2f333936e21e9efe9f2be77c91
staveesh <sharmataveesh@yahoo.in>
2021-04-03 09:10:10 +0200

    CORS issue check

--- src/main/java/com/taveeshsharma/requesthandler/controllers/WebSocketController.java
+++ src/main/java/com/taveeshsharma/requesthandler/controllers/WebSocketController.java
@@ -12,7 +12,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Date;
 
