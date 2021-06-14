6ff7895e2139a7e49d92f4b8156dc21e7d4cf30e
staveesh <sharmataveesh@yahoo.in>
2021-06-14 16:23:19 +0200

    Testing RR 0.9

--- src/main/java/com/taveeshsharma/requesthandler/config/NetworkTopologyConfig.java
+++ src/main/java/com/taveeshsharma/requesthandler/config/NetworkTopologyConfig.java
@@ -27,7 +27,7 @@ public class NetworkTopologyConfig {
     private static final Logger logger = LoggerFactory.getLogger(NetworkTopologyConfig.class);
 
     public NetworkGraph createOrImportGraph() throws IOException {
-        NetworkGraph graph = new NetworkGraph(8,4,0.5);
+        NetworkGraph graph = new NetworkGraph(8,4,0.9);
         // TODO: Move this path to properties file
         File topFile = new File("/var/lib/graphs/topology.dot");
         if(topFile.exists()) {
