ad4b2245504d91ab768f6cd7b54ecc4fab3de2d1
staveesh <sharmataveesh@yahoo.in>
2021-06-17 19:52:34 +0200

    Added randomization logic

--- src/main/java/com/taveeshsharma/requesthandler/config/NetworkTopologyConfig.java
+++ src/main/java/com/taveeshsharma/requesthandler/config/NetworkTopologyConfig.java
@@ -27,7 +27,7 @@ public class NetworkTopologyConfig {
     private static final Logger logger = LoggerFactory.getLogger(NetworkTopologyConfig.class);
 
     public NetworkGraph createOrImportGraph() throws IOException {
-        NetworkGraph graph = new NetworkGraph(8,4,0.2);
+        NetworkGraph graph = new NetworkGraph(8,4,0.1);
         // TODO: Move this path to properties file
         File topFile = new File("/var/lib/graphs/topology.dot");
         if(topFile.exists()) {
